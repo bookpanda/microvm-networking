fde7210339255803c35b499ddd2bfacfcf4bb4c2
Idhibhat Pankam <marginpankam@gmail.com>
2025-09-10 23:39:23 +0700

    d

---
+++ firecracker.md
@@ -0,0 +1,5 @@
+```bash
+cargo build --release --no-default-features
+./target/release/firecracker --no-kvm -n --config-file path/to/config.json
+
+```
--- firecracker/modules/ec2/ec2.tf
+++ firecracker/modules/ec2/ec2.tf
@@ -16,20 +16,7 @@ resource "aws_instance" "firecracker_ec2" {
 
   security_groups = [aws_security_group.sg_firecracker.id]
   key_name        = aws_key_pair.generated_key_pair.key_name
-  #   user_data = templatefile("${path.module}/install_wordpress.sh", {
-  #     DB_NAME        = "${var.database_name}"
-  #     DB_USER        = "${var.database_user}"
-  #     DB_PASS        = "${var.database_pass}"
-  #     DB_HOST        = "${aws_network_interface.eni_db_app_inet.private_ip}:3306"
-  #     DB_PREFIX      = "wp_"
-  #     WP_URL         = "http://${aws_eip.app_inet_eip.public_ip}"
-  #     WP_ADMIN_USER  = "${var.admin_user}"
-  #     WP_ADMIN_PASS  = "${var.admin_pass}"
-  #     WP_ADMIN_EMAIL = "${var.admin_email}"
-  #     WP_TITLE       = "Cloud"
-  #     REGION         = "${var.region}"
-  #     BUCKET_NAME    = "${var.bucket_name}"
-  #   })
+  user_data       = templatefile("${path.module}/user_data.sh", {})
 
   tags = {
     Name = "firecracker-ec2"
