a67ad442aa287ae1d967b88a6ecf4538dc82118a
Idhibhat Pankam <marginpankam@gmail.com>
2025-10-04 22:41:16 +0700

    add more code

--- cloudlab/init-2-code.sh
+++ cloudlab/init-2-code.sh
@@ -14,24 +14,32 @@ fi
 
 git config --global user.name "$NAME"
 git config --global user.email "$EMAIL"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/github
+ssh -T git@github.com
 
 # go
 wget https://go.dev/dl/go1.23.7.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf go1.23.7.linux-amd64.tar.gz
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
-source ~/.bashrc
+export PATH=$PATH:/usr/local/go/bin # make it work right now
 go version
+rm go1.23.7.linux-amd64.tar.gz
 
 # linux kernel
-curl -fsSL -o /tmp/vmlinux-5.10.223-no-acpi http://spec.ccfc.min.s3.amazonaws.com/firecracker-ci/v1.10/x86_64/vmlinux-5.10.223-no-acpi
+echo "Downloading linux kernel..."
+curl -fL --progress-bar -o /tmp/vmlinux-5.10.223-no-acpi http://spec.ccfc.min.s3.amazonaws.com/firecracker-ci/v1.10/x86_64/vmlinux-5.10.223-no-acpi
 # rootfs
-curl -fsSL -o /tmp/debian-rootfs.ext4 https://cloudlab-microvm.s3.ap-southeast-1.amazonaws.com/debian-rootfs.ext4
+echo "Downloading rootfs..."
+# curl -fL --progress-bar -o /tmp/debian-rootfs.ext4 https://cloudlab-microvm.s3.ap-southeast-1.amazonaws.com/debian-rootfs.ext4
 
 # code
 mkdir -p ~/code
 cd ~/code
 git clone git@github.com:bookpanda/microvm-networking.git
-git clone git@github.com:bookpanda/firecracker-runner-node.git
+git clone git@github.com:bookpanda/firecracker-runner-node.git runner-node
+git clone git@github.com:bookpanda/firecracker-vsock.git vsock
+git clone git@github.com:bookpanda/userspace-stack.git
 
 cp ~/code/microvm-networking/cloudlab/config ~/.ssh/config
