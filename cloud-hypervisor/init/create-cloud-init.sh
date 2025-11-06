1afb77a10cfd84b5112ce7927bb654aa7c050b54
Idhibhat Pankam <marginpankam@gmail.com>
2025-11-06 09:54:32 -0700

    netplan

--- cloud-hypervisor/init/create-cloud-init.sh
+++ cloud-hypervisor/init/create-cloud-init.sh
@@ -6,3 +6,15 @@ mkdosfs -n CIDATA -C /tmp/ubuntu-cloudinit.img 8192
 mcopy -oi /tmp/ubuntu-cloudinit.img -s ./init/user-data ::
 mcopy -oi /tmp/ubuntu-cloudinit.img -s ./init/meta-data ::
 mcopy -oi /tmp/ubuntu-cloudinit.img -s ./init/network-config ::
+
+rm -f /tmp/cloudinit-vm0.img
+mkdosfs -n CIDATA -C /tmp/cloudinit-vm0.img 8192
+mcopy -oi /tmp/cloudinit-vm0.img -s ./init/user-data ::
+mcopy -oi /tmp/cloudinit-vm0.img -s ./init/meta-data ::
+mcopy -oi /tmp/cloudinit-vm0.img -s ./init/network-config-vm0 ::
+
+rm -f /tmp/cloudinit-vm1.img
+mkdosfs -n CIDATA -C /tmp/cloudinit-vm1.img 8192
+mcopy -oi /tmp/cloudinit-vm1.img -s ./init/user-data ::
+mcopy -oi /tmp/cloudinit-vm1.img -s ./init/meta-data ::
+mcopy -oi /tmp/cloudinit-vm1.img -s ./init/network-config-vm1 ::
