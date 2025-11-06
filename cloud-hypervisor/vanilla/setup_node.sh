1ed89474435fca0eae18b7bd7b71d66321f8c359
Idhibhat Pankam <marginpankam@gmail.com>
2025-11-06 10:46:06 -0700

    setup node

--- cloud-hypervisor/vanilla/setup_node.sh
+++ cloud-hypervisor/vanilla/setup_node.sh
@@ -26,6 +26,21 @@ sudo mkdir -p /etc/qemu
 sudo bash -c 'echo "allow br0" > /etc/qemu/bridge.conf'
 echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward
 
+# Clean up OvS bridge
+for br in $(ovs-vsctl list-br 2>/dev/null || true); do
+    sudo ip link set "$br" down || true
+    sudo ovs-vsctl del-br "$br" || true
+done
+
+# create tap0, br0
+sudo ip link add name br0 type bridge || true
+sudo ip link set br0 up || true
+sudo ip addr add 192.168.10${NODE_ID}.1/24 dev br0 || true
+
+sudo ip tuntap add dev tap0 mode tap user $USER || true
+sudo ip link set tap0 master br0 || true
+sudo ip link set tap0 up || true
+
 # Apply netplan config
 sudo netplan apply --file ./netplan-node${NODE_ID}.yaml
 
