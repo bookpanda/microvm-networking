bf7c317099131fa7ee122a346b434cbbc1096221
Idhibhat Pankam <marginpankam@gmail.com>
2025-11-20 01:53:12 -0700

    change name

--- cloud-hypervisor/init/clean-disk-state.sh
+++ cloud-hypervisor/init/clean-disk-state.sh
@@ -1,5 +1,5 @@
 sudo modprobe nbd max_part=8
-sudo qemu-nbd -c /dev/nbd0 -f raw /tmp/focal-server-cloudimg-amd64.raw
+sudo qemu-nbd -c /dev/nbd0 -f raw /tmp/noble-server-cloudimg-amd64.raw
 sudo mount /dev/nbd0p1 /mnt
 sudo rm -rf /mnt/var/lib/cloud/instances /mnt/var/lib/cloud/instance /mnt/var/lib/cloud/data
 sudo umount /mnt
